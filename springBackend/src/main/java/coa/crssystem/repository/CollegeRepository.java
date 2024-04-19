abec63799d47d73660c08270bbc403d1fd805cbc
msrosales <msrosales@up.edu.ph>
2024-04-20 00:55:20 +0800

    moving on stage

--- springBackend/src/main/java/coa/crssystem/repository/CollegeRepository.java
+++
@@ -1,9 +0,0 @@
-package coa.crssystem.repository;
-
-import coa.crssystem.entity.College;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CollegeRepository  extends JpaRepository<College,Long> {
-
-College findByCollegeId(Long collegeId);
-}
--- springBackend/src/test/java/coa/crssystem/SpringBackendApplicationTests.java
+++
@@ -1,6 +0,0 @@
-package coa.crssystem;
-
-class SpringBackendApplicationTests {
-
-
-}
